75ca842b48434e3ee6943f2454d63d75a2cc3f57
phreno <losgoomy@gmail.com>
2022-09-12 21:47:33 +0200

    Rend la rose dynamique

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -1,26 +1,41 @@
 export default function sketch(s) {
-  let x, y, backgroundColor;
-
-  const width = 500;
-  const height = 500;
+  const BACKGROUND = 0;
+  const STROKE = 255;
+  const FILL = 122;
+  const SIZE = 500;
+  const CENTER = SIZE / 2;
+  const FRAME_RATE = 30;
+  const STEPS = 3000;
+  const ROSE_SIZE = 100
+  let ROSE_AMPLITUDE = 0.001
+  let INCREMENT = 0.0001
 
   s.setup = () => {
-    s.createCanvas(width, height);
-    backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
-
-    x = s.random(width);
-    y = height / 2;
+    s.createCanvas(SIZE, SIZE);
   };
 
   s.draw = () => {
-    s.background(backgroundColor);
-    s.fill(s.color(255, 0, 0));
-    s.ellipse(x, y, 100, 100);
-
-    x = (x + 1) % width;
+    s.translate(CENTER, CENTER)
+    s.background(BACKGROUND);
+    s.beginShape();
+    s.noFill();
+    [...Array(STEPS).keys()]
+      .map(n => s.map(n, 0, STEPS, 0, s.TWO_PI * 15))
+      .forEach(theta => {
+        let r = ROSE_SIZE * s.cos(ROSE_AMPLITUDE * theta);
+        // let x = SIZE / 2 + r * s.cos(theta);
+        // let y = SIZE / 2 + r * s.sin(theta);
+        console.log(theta);
+        // let r = 100;
+        let x = r * s.cos(theta);
+        let y = r * s.sin(theta);
+        s.stroke(STROKE);
+        s.vertex(x, y);
+      });
+    s.endShape();
+    ROSE_AMPLITUDE += INCREMENT;
   };
 
   s.mousePressed = () => {
-    backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
   };
 }
